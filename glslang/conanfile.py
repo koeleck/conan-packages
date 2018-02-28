6cfedc1f7e34f8ff52f0d656f33a227ab99b49ec
Philipp Kögel <philippkoegel@gmail.com>
2018-02-28 20:52:48 +0100

    Fix windows build, add revision, and update info

--- glslang/conanfile.py
+++ glslang/conanfile.py
@@ -4,30 +4,30 @@ import os, shutil
 
 class GlslangConan(ConanFile):
     name = "glslang"
+    _revision = '2651ccaec8170b3257642b3c438f50dc4f181fdd'
     version = "2651cca"
     license = "3-Clause BSD"
-    url = "<Package recipe repository url here, for issues about the package>"
+    url = "https://github.com/koeleck/conan-packages/tree/master/glslang"
     description = "glslang package for conan "
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
     requires = 'spirv-tools/2018.0@koeleck/testing'
 
     def source(self):
-        revision = '2651ccaec8170b3257642b3c438f50dc4f181fdd'
-        download('https://github.com/KhronosGroup/glslang/archive/{}.zip'.format(revision), 'glslang.zip')
+        download('https://github.com/KhronosGroup/glslang/archive/{}.zip'.format(self._revision), 'glslang.zip')
         check_sha256('glslang.zip', 'ad0950a61247ca66526e5f6fd3684dd6d7502083e9751871a7b4ea40d06400da')
         unzip('glslang.zip')
         os.unlink('glslang.zip')
-        shutil.move('glslang-{}'.format(revision), 'glslang')
+        shutil.move('glslang-{}'.format(self._revision), 'glslang')
         # This small hack might be useful to guarantee proper /MT /MD linkage in MSVC
         # if the packaged project doesn't have variables to set it properly.
         # Also: Define target 'SPIRV-Tools-opt' to enable the optimization passes
         tools.replace_in_file("glslang/CMakeLists.txt", "project(glslang)", '''project(glslang)
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup(TARGETS)
+conan_basic_setup()
 add_library(SPIRV-Tools-opt INTERFACE)
-target_link_libraries(SPIRV-Tools-opt INTERFACE CONAN_PKG::spirv_tools)
-target_include_directories(SPIRV-Tools-opt INTERFACE "${CONAN_INCLUDE_DIRS_SPIRV-TOOLS}/spirv-tools")''')
+target_link_libraries(SPIRV-Tools-opt INTERFACE ${CONAN_LIBS_SPIRV-TOOLS})
+target_include_directories(SPIRV-Tools-opt INTERFACE "${CONAN_INCLUDE_DIRS_SPIRV-TOOLS}" "${CONAN_INCLUDE_DIRS_SPIRV-TOOLS}/spirv-tools")''')
 
     def build(self):
         cmake = CMake(self)
@@ -42,6 +42,9 @@ target_include_directories(SPIRV-Tools-opt INTERFACE "${CONAN_INCLUDE_DIRS_SPIRV
         self.copy("*.h*", dst="include/hlsl", src="{}/hlsl/".format(base_dir))
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
+
+        with open('{}/revision.txt'.format(self.package_folder), 'w') as rev_file:
+            rev_file.write(self._revision)
 
     def package_info(self):
         self.cpp_info.libs = ['glslang',
