d48a1859278c92ed74e63c4e982d185a5cbb58e3
Philipp Kögel <philipp.koegel@elektrobit.com>
2018-03-02 16:05:00 +0100

    VulkanTools: Cleanup and install json files

--- VulkanTools/conanfile.py
+++ VulkanTools/conanfile.py
@@ -1,6 +1,5 @@
 from conans import ConanFile, CMake, tools
-from conans.tools import download, unzip, check_sha256
-import os, shutil, errno
+import os, errno, glob, re
 
 def _create_directory(path):
     try:
@@ -13,7 +12,7 @@ def _create_directory(path):
 def _make_empty_file(path):
     parent_dir = os.path.dirname(path)
     _create_directory(parent_dir)
-    with open(path, 'w') as fp:
+    with open(path, 'w') as _:
         pass
 
 class VulkantoolsConan(ConanFile):
@@ -30,12 +29,6 @@ class VulkantoolsConan(ConanFile):
 
     def source(self):
         revision = '2fbb616a7fe0230b3255cd0c5307fcd8551f3b1d'
-        #download('https://github.com/LunarG/VulkanTools/archive/sdk-{}.zip'.format(self.version), 'sdk.zip')
-        #check_sha256('sdk.zip', '4bb35ed5334a443e43bf3abfc06701724218cbe1b0fa80d87cc3fdd76d4c746f')
-        #unzip('sdk.zip')
-        #os.unlink('sdk.zip')
-        #unzip('D:\\Downloads\\tmp\\VulkanTools-sdk-1.0.68.0.zip')
-        #shutil.move('VulkanTools-sdk-{}'.format(self.version), 'sdk')
         self.run('git clone https://github.com/LunarG/VulkanTools.git')
         self.run('cd VulkanTools && git reset --hard {} && git submodule init && git submodule update'.format(revision))
         self.run('cd VulkanTools/submodules/jsoncpp && python amalgamate.py')
@@ -67,26 +60,38 @@ conan_basic_setup(NO_OUTPUT_DIRS)''')
         self.copy('vk_dispatch_table_helper.h', dst='include/vulkan', src='{}/submodules/Vulkan-LoaderAndValidationLayers'.format(self.build_folder), keep_path=False)
         self.copy('vk_layer_dispatch_table.h', dst='include/vulkan', src='{}/submodules/Vulkan-LoaderAndValidationLayers'.format(self.build_folder), keep_path=False)
 
-        #TODO json files
         extra_path = '/{}'.format(self.settings.build_type) if self.settings.os == 'Windows' else ''
-        paths = ['layersvt', 'submodules/Vulkan-LoaderAndValidationLayers/layers',
-                 'submodules/Vulkan-LoaderAndValidationLayers/loader']
-        for p in paths:
+        layer_input_paths = ['layersvt', 'submodules/Vulkan-LoaderAndValidationLayers/layers']
+        library_input_paths = layer_input_paths + ['submodules/Vulkan-LoaderAndValidationLayers/loader']
+        for p in library_input_paths:
             src = '{}/{}{}'.format(self.build_folder, p, extra_path)
-            self.copy('*.lib', dst='lib', src=src, keep_path=False)
+            self.copy('*.lib', dst='lib', src=src, keep_path=False, excludes='VKstatic.*')
             self.copy('*.dll', dst='bin', src=src, keep_path=False)
             self.copy('*.so', dst='lib', src=src, keep_path=False)
             self.copy('*.dylib', dst='lib', src=src, keep_path=False)
             self.copy('*.a', dst='lib', src=src, keep_path=False)
+
+        json_dst = '{}/layers.d'.format(self.package_folder)
+        _create_directory(json_dst)
+        for p in layer_input_paths:
+            pattern = '{}/{}{}/*.json'.format(self.build_folder, p, extra_path)
+            for json in glob.glob(pattern):
+                self._copy_layer_json(dst='{}/{}'.format(json_dst, os.path.basename(json)),
+                                      src=json)
+
         src = '{}/submodules/Vulkan-LoaderAndValidationLayers{}'.format(self.build_folder, extra_path)
-        self.copy('VkLayer_utils.lib', dst='lib', src=src, keep_path=False)
-        self.copy('VkLayer_utils.dll', dst='bin', src=src, keep_path=False)
-        self.copy('VkLayer_utils.so', dst='lib', src=src, keep_path=False)
-        self.copy('VkLayer_utils.dylib', dst='lib', src=src, keep_path=False)
-        self.copy('VkLayer_utils.a', dst='lib', src=src, keep_path=False)
+        self.copy('*VkLayer_utils.lib', dst='lib', src=src, keep_path=False)
+        self.copy('*VkLayer_utils.dll', dst='bin', src=src, keep_path=False)
+        self.copy('*VkLayer_utils.so', dst='lib', src=src, keep_path=False)
+        self.copy('*VkLayer_utils.dylib', dst='lib', src=src, keep_path=False)
+        self.copy('*VkLayer_utils.a', dst='lib', src=src, keep_path=False)
+
 
     def package_info(self):
-        self.cpp_info.libs = ['vulkan-1']
+        vulkan_lib = 'vulkan-1' if self.settings.os == 'Windows' else 'vulkan'
+        self.cpp_info.libs = [vulkan_lib]
+        self.cpp_info.defines = ['VULKAN_TOOLS_VK_LAYER_PATH={}{}layers.d'.format(
+                self.cpp_info.rootpath, os.path.sep)]
 
     # Helper
     def _generate_commit_id_header(self):
@@ -104,4 +109,21 @@ conan_basic_setup(NO_OUTPUT_DIRS)''')
         _create_directory('{}/submodules/Vulkan-LoaderAndValidationLayers/'.format(self.build_folder))
         with open(commit_id_header, 'w') as out_file:
             out_file.write('#pragma once\n#define SPIRV_TOOLS_COMMIT_ID "{}"'.format(revision))
+
+    def _adjust_json_line(self, line):
+        match = re.fullmatch(r'^(.*)"library_path":[ \t]*".*?((?:lib|)VkLayer[^"]*)"(.*\n?)$', line)
+        if match:
+            return '{}"library_path": "..{sep}{}{sep}{}"{}'.format(
+                    match.group(1),
+                    'bin' if self.settings.os == 'Windows' else 'lib',
+                    match.group(2),
+                    match.group(3),
+                    sep=os.path.sep)
+        return line
+
+    def _copy_layer_json(self, *, dst, src):
+        with open(dst, 'w') as out_file:
+            with open(src, 'r') as in_file:
+                for line in in_file:
+                    out_file.write(self._adjust_json_line(line))
 
--- VulkanTools/test_package/example.cpp
+++ VulkanTools/test_package/example.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-#include "hello.h"
+#include <vulkan/vulkan.h>
 
 int main() {
-    hello();
 }
